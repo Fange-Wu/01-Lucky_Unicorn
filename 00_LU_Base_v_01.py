8c228e8a4af70fde310d794a8c070ae8248c1bf7
Fange-Wu <fange.wu@liston.school.nz>
2021-05-11 12:07:53 +1200

    Made a token generator loops

--- 00_LU_Base_v_01.py
+++ 00_LU_Base_v_01.py
@@ -18,18 +18,49 @@ def yes_no(question):
         else: 
             print("Please answer yes / no ")
 
-# Main Routine goes here...
+
 def instructions () :
+
     print("**** How to Play ****")
     print()
     print("The rules of the game go here")
     print()
     return ""
 
+
+def num_check(question, low, high):
+    error = "Please enter an whole number between 1 and 10\n"
+
+    valid = False 
+    while not valid:
+        try:
+            # question
+            response = int(input(question))
+
+            # if the amount is too low or too high
+            if low < response <= high:
+                return response
+
+
+            # output an error
+            else:
+                print(error)
+        
+        except ValueError:
+            print(error)
+
+# Main Routine
 show_instructions = yes_no("Have you played the game before? ")
 
 if show_instructions == "no":
     instructions()
 
 print("Program Continues")
+print()
+
+# Ask user how much they want to play with
+how_much = num_check ("How much would you like to play with? ", 0, 10)
+
+
+print("You will be spending ${}" .format(how_much))
 
