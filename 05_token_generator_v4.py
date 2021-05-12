90842ca0ec10b548241b4977f53307302bf4e744
Fange-Wu <fange.wu@liston.school.nz>
2021-05-12 12:42:49 +1200

    token generator based of percentage chance now

--- 05_token_generator_v4.py
+++ 05_token_generator_v4.py
@@ -1,8 +1,7 @@
 import random
 
-
 # main routine goes here
-tokens = ["unicorn", "horse", "horse", "horse","zebra", "zebra", "zebra", "donkey", "donkey", "donkey"]
+
 STARTING_BALANCE = 100
 
 balance = STARTING_BALANCE
@@ -10,14 +9,17 @@ balance = STARTING_BALANCE
 
 # Testing loop to genereate 20 tokens
 for item in range (0,100) :
-    chosen = random.choice(tokens)
+    chosen_num = random.randint(1, 100)
 
     # Adjust balance
-    if chosen == "unicorn":
+    if 1 <= chosen_num <=5 :
+        chosen = "unicorn"
         balance += 4
-    elif chosen == "donkey":
+    elif 6 <= chosen_num <=36 :
+        chosen = "donkey"
         balance -= 1
     else:
+        chosen= "horse" / "zebra"
         balance -= 0.5
 
     
